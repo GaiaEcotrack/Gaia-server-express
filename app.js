09c565548450347f38d76e91a1ce50a3142f7f98
Nicolas <villagrajosenicolas@gmail.com>
2024-07-01 15:56:18 -0300

    feat:JWT

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const updateFunctions = require('./controllers/updateKwController')
-const sendContracMessage = require('./controllers/tokenController')
+const updateFunctions = require('./controllers/updateKwController');
+const sendContracMessage = require('./controllers/tokenController');
 const verifyToken = require('./middlewares/authMiddleware');
 const cron = require('node-cron');
 
@@ -14,90 +14,81 @@ const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+
 const corsOptions = {
     origin: '*',
-    methods: ['GET', 'POST','PUT','DELETE'],
-    allowedHeaders: ['Content-Type'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Length'],
     credentials: true
-  };
-  
-  app.use(cors(corsOptions));
+};
+
+app.use(cors(corsOptions));
+
 // Import routes
-// const userRoutes = require('./routes/userRoutes');
-// const productRoutes = require('./routes/productRoutes');
 const apiRoutes = require('./routes/device');
 const generadorRoutes = require('./routes/generadorRoutes');
 const credencialsUser = require('./routes/credencialsRoutes');
+const authRoutes = require('./routes/authRoutes');
 
-// // Use routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/products', productRoutes);
-let shouldExecute = false;
+// Use routes
 app.use('/api', apiRoutes);
 app.get("/api", (req, res) => {
     res.send("App running 👍");
-  });
-app.use('/generator', generadorRoutes);
-app.use('/credencials',credencialsUser)
-
-let isUpdating = false
-// Función para actualizar usuarios
-async function startUpdatingUsers() {
-  try {
-      isUpdating = true; // Indicar que se está actualizando
-      await updateFunctions.actualizarKwGeneradoParaUsuarios();
-      console.log('Proceso de actualización completado.');
-  } catch (error) {
-      console.error('Error durante la actualización de usuarios:', error.message);
-  } finally {
-      isUpdating = false; // Indicar que se ha finalizado la actualización
-  }
-}
-
-// Ruta GET para iniciar el proceso de actualización
-app.get('/update-users', async (req, res) => {
-  if (isUpdating) {
-      res.send('Se está realizando el proceso de actualización.');
-  } else {
-      await startUpdatingUsers();
-      res.send('Se terminó el proceso de actualización.');
-  }
 });
+app.use('/generator', verifyToken, generadorRoutes);
+app.use('/credencials', verifyToken, credencialsUser);
+app.use('/auth', authRoutes);
+
+let isUpdating = false;
+
+// Función para actualizar usuarios
+// async function startUpdatingUsers() {
+//     try {
+//         isUpdating = true; // Indicar que se está actualizando
+//         await updateFunctions.actualizarKwGeneradoParaUsuarios();
+//         console.log('Proceso de actualización completado.');
+//     } catch (error) {
+//         console.error('Error durante la actualización de usuarios:', error.message);
+//     } finally {
+//         isUpdating = false; // Indicar que se ha finalizado la actualización
+//     }
+// }
+
+// // Ruta GET para iniciar el proceso de actualización
+// app.get('/update-users', async (req, res) => {
+//     if (isUpdating) {
+//         res.send('Se está realizando el proceso de actualización.');
+//     } else {
+//         await startUpdatingUsers();
+//         res.send('Se terminó el proceso de actualización.');
+//     }
+// });
 
 app.get('/api/protected-route', verifyToken, (req, res) => {
-  // req.user contiene la información decodificada del token
-  res.json({ message: 'This is a protected route', user: req.user });
+    // req.user contiene la información decodificada del token
+    res.json({ message: 'This is a protected route', user: req.user });
 });
-
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
-
-//     async function startUpdatingUsers() {
-//       try {
-//           await updateFunctions.actualizarKwGeneradoParaUsuarios();
-//           console.log('Esperando 10 segundos antes de la siguiente actualización...');
-//       } catch (error) {
-//           console.error('Error durante la actualización de usuarios:', error.message);
-//       } finally {
-//           setTimeout(startUpdatingUsers, 10000); // Esperar 10 segundos y ejecutar de nuevo
-//       }
-//   }
-  
-//   // Start updating users
+// Start updating users
 cron.schedule('00 20 * * *', async () => {
-  console.log('Iniciando la actualización de usuarios programada.');
-  await startUpdatingUsers();
+    console.log('Iniciando la actualización de usuarios programada.');
+    await startUpdatingUsers();
 }, {
-  scheduled: true,
-  timezone: "America/Bogota" // Zona horaria de Colombia
+    scheduled: true,
+    timezone: "America/Bogota" // Zona horaria de Colombia
 });
-// // sendContracMessage.sendMessageContract()
-const PORT = process.env.PORT || 5100;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app;;
+const IP = '0.0.0.0'; // Cambia esta IP por la correcta de tu servidor
+const PORT = process.env.PORT || 5100;
+
+app.listen(PORT, IP, () => {
+    console.log(`Server running on http://${IP}:${PORT}`);
+});
+
+module.exports = app;
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
+function verifyToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).send('Access denied. No token provided.');
+    }
 
-  if (!token) {
-    return res.status(401).json({ message: 'Access Denied: Token not provided' });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user; // Guardar el usuario decodificado en el request
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(401).json({ message: 'Invalid Token' });
-  }
-};
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send('Invalid token.');
+        }
+        req.user = decoded;
+        next();
+    });
+}
 
 module.exports = verifyToken;
