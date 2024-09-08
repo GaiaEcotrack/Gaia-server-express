4e935e7ae45a8331c6db1749699faf22515c74de
Nicolas <villagrajosenicolas@gmail.com>
2024-09-08 20:12:08 -0300

    update

--- controllers/comercialUsersController.js
+++ controllers/comercialUsersController.js
@@ -1,4 +1,4 @@
-const User = require('../models/comercialUser'); // Ajusta la ruta según tu estructura de archivos
+ const User = require('../models/comercialUser'); // Ajusta la ruta según tu estructura de archivos
 
 // Crear un nuevo usuario
 exports.createUser = async (req, res) => {
